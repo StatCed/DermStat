c0b88c6db92ce57923251783e45427616b32b1b9
Cédric JUNG <CJU@dermscan.com>
2021-05-05 13:24:26 +0200

    import.py: Lettre du lecteur réseau en fonction du site + Mise à jour

--- 2.Analyse/0.Data/0.DataCSV/import.py
+++ 2.Analyse/0.Data/0.DataCSV/import.py
@@ -1,30 +1,42 @@
 import os, csv, datetime
 
-print('Hello\nScript de création de fichier SAS d importation de données\n\n\n')
+print('############################################################')
+print('#                                                          #')
+print('# Création du fichier .SAS pour importation du ficher .CSV #')
+print('# contenant les données brutes                             #')  
+print('#                                                          #')
+print('############################################################\n\n')
+
 
 #Date de création du fichier
 date=str(datetime.datetime.now())
 
+
 #Adresse repertoire courant
 adresse=os.getcwd();
-print("Adresse du répertoire pour windows :\n",adresse,"\n")
-#Modification de la lettre du disque réseau pour correspondre au  disque local
-#du serveur
-# Q: -> D:
+#print("Adresse du répertoire pour windows :\n",adresse,"\n")
 
-adresseSAS = adresse.replace('Q:' , 'D:')
+
+#Modification de la lettre du disque réseau pour correspondre au  disque local
+#du serveur en focntion du site (Tunis ou Lyon) -> test sur l utilisateur
+# Q: -> D:
+if os.getlogin() == 'CJU':
+	adresseSAS = adresse.replace('Q:' , 'D:')
+else:
+	adresseSAS = adresse.replace('Q:' , 'W:')
     
-print("Le repertoire contenant les données brutes :\n",adresseSAS,"\n")
+#print("Le repertoire contenant les données brutes :\n",adresseSAS,"\n")
+
 
 #Chemin du driver SAS
 driver=adresseSAS.replace("\\0.Data\\0.DataCSV", "\\0.Driver.sas")
-print("Le chemin du Driver :\n",driver,"\n")
+#print("Le chemin du Driver :\n",driver,"\n")
 
-#Non du fichier brut données brutes .csv     
-donnees = input('\nNom du fichier de donnéees brute en .csv:')
-position_colonne_produit = input('\nPosition de la colonne produit :')
-position_colonne_produit = int(position_colonne_produit)
-plan = input("\nPlan expérimental (Intra/Para/BIE) :")
+
+#Information relative à l étude à renseigner  
+donnees = input('\nNom du fichier de donnéees brute en .csv: ')
+
+plan = input("\nPlan expérimental (Intra/Para/BIE) : ")
 
 infos_etude = input("\nSouhaitez-vous rentrer les informations relatives à l'étude (O/N):")
 if infos_etude.upper()=='O':
@@ -38,12 +50,14 @@ if infos_etude.upper()=='O':
 
 #Chemin du fichier brut   
 adresse_donnees = adresseSAS + "\\" + donnees + ".csv"
-print("Le chemin des données brutes :\n",adresse_donnees,"\n")
+#print("\n\nLe chemin des données brutes :\n",adresse_donnees,"\n")
+
 
 #Nom du fichier SAS d importation   
 import_sas = "I" + donnees + ".sas"
 
 
+#Ecriture du code SAS 
 file=donnees+".csv"
 with open(file) as csv_file:
     with open(import_sas, "w+", encoding="ANSI") as imp_new:
@@ -61,22 +75,22 @@ with open(file) as csv_file:
 %let date = {date};\n\n'                 
         )
         imp_new.write(f'%include "{driver}"; \n\n\
-            DATA lib.R{donnees}; \n\
-            infile "{adresse_donnees}" \n\
-            LRECL=32767 \n\
-            ENCODING=wlatin1 \n\
-            firstobs=2 \n\
-            TERMSTR=CRLF \n\
-            DLM=";" \n\
-            MISSOVER \n\
-            DSD ; \n\
-            INPUT \n')
+DATA lib.R{donnees}; \n\
+	infile "{adresse_donnees}" \n\
+	LRECL=32767 \n\
+	ENCODING=wlatin1 \n\
+	firstobs=2 \n\
+	TERMSTR=CRLF \n\
+	DLM=";" \n\
+	MISSOVER \n\
+	DSD ; \n\
+INPUT \n')
         csv_reader= csv.reader(csv_file, delimiter=';')
         line_count = 1
         for row in csv_reader:
             if line_count == 1:
                 for variable in row:
-                    if line_count == position_colonne_produit:
+                    if line_count == 1:
                         imp_new.write(f'{variable} : $char100. \n')
                         line_count += 1
                     else:
@@ -85,25 +99,25 @@ with open(file) as csv_file:
                 imp_new.write(f';\nrun;\n\n\n')
 
                 imp_new.write(f'data lib.R{donnees};\n\
-retain product subject;\n\
-set lib.R{donnees};\n\
-run;\n')
+	retain product subject;\n\
+	set lib.R{donnees};\n\
+run;\n\n')
                 
                 imp_new.write(f'\noptions fmtsearch=(env.F{donnees});\n\
 %table2(\n\
-                    tableEntree= lib.R{donnees} , \n\
-                    tableSortie=lib.D{donnees},   \n\
-                    autresVariables=,  \n\
-                    vartransposedeb=3,  \n\
-                    listeFormatsParametre = , \n\
-                    listeProduits = ,	\n\
-                    listeFormatsProduit = ,	 \n\
-                    listeTemps = ,	         \n\
-                    listeTempsNum = ,	 \n\
-                    TypeEtude     =  {plan}     , \n\
-                    delta= abs 	,       \n\
-                    libFormat = env.F{donnees},           \n\
-                    tableVar =  env.E{donnees});\n\n\n')
+	tableEntree = lib.R{donnees}, 	\n\
+	tableSortie = lib.D{donnees},   \n\
+	autresVariables = ,  			\n\
+	listeParametres = , 			\n\
+	listeFormatsParametre = , 		\n\
+	listeProduits = ,				\n\
+	listeFormatsProduit = ,			\n\
+	listeTemps = ,	         		\n\
+	listeTempsNum = ,	 			\n\
+	TypeEtude =  {plan}, 			\n\
+	delta = oui,       				\n\
+	libFormat = env.F{donnees},     \n\
+	tableVar =  env.E{donnees});\n\n\n')
                 
                                          				           
-                Stop = input("\n\nCréation du fichier d'import terminé !")
+Stop = input("\n\nCréation du fichier d'import terminé !")
